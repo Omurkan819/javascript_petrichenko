b868731cc02305ac81b04da7a6540efa96e0af75
Omurkan <oabaskanov@mail.ru>
2020-05-04 18:21:09 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,55 @@
+// 'use strict';
+
+a = 5;
+
+// var leftBorderWidth = 1; {
+//   let second = 2;
+// }
+// const pi = 3.14;
+// var df;
+// console.log(3 / 0);
+// console.log('tree' * 5);
+// console.log(df);
+
+// let person = {
+//   name: "John",
+//   age: 23,
+//   isMarried: false
+// }
+
+// console.log(person.name);
+// console.log(person["name"]);
+
+// let arr = ['plum.png', 'orange', 'apple.tmp'];
+
+// console.log(arr[2]);
+
+// alert('Privet.');
+
+// let answer = confirm("Ты еще здесь?");
+// console.log(answer);
+
+// let answer = +prompt("Тебе уже есть 18 лет?", 'ДА');
+// console.log(answer);
+
+// console.log(typeof (answer));
+
+// console.log("arr" + " - object");
+// // console.log(4 + +" - object");
+
+// let incr = 10;
+// decr = 10;
+
+
+// console.log(++incr);
+// console.log(decr--);
+// console.log(incr++);
+
+// console.log(10 % 7);
+
+// console.log("asw" == "as");
+
+let isChecked = false;
+isClose = false;
+
+console.log(isChecked || isClose);
